56dc66406b7c6b4155911fc85e0bfc070ab22b61
fando01 <gabbxfiles@gmail.com>
2016-11-02 16:29:44 +0400

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var config = {
   // - Your app's id on moneypot.com
   app_id: 2381,                             // <----------------------------- EDIT ME!
   // - Displayed in the navbar
-  app_name: '5050 Bit Chance',
+  app_name: '50/50 Bit Chance by MoneyPot',
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
   recaptcha_sitekey: '6Lf97AoUAAAAAIJMrau0EvFmyi2vv1r1DsPvjQyj',  // <----- EDIT ME!
